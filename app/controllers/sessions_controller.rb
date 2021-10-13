bce2ccdd31fb46c3df5434ea23ca54294abdcb9b
Erin <errose10@gmail.com>
2021-10-12 20:09:57 -0400

    create login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,25 @@ class SessionsController < ApplicationController
 
     def welcome
     end
+
+    def destroy
+        session.delete(:user_id)
+        redirect_to '/'
+    end
+
+    def new
+    end
+
+    def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user.try(authenticate, params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:error] = "Invalid login, please try again."
+            redirect_to login_path
+        end
+    end
+
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  delete '/logout' => 'sessions#destroy'
 
   resources :ratings
   resources :users
